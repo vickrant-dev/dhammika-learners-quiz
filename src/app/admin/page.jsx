ff919025e2b12b4c7430e9332405c1d62d4e7fad
Vickrant <vickrantselvendran@gmail.com>
2025-05-08 10:42:13 +0530

    Fixed err...

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,18 +1,10 @@
-// 'use client'
+'use client'
 
-import Admin from "../Components/Admin/Admin"
+import dynamic from 'next/dynamic';
 
-// import dynamic from 'next/dynamic';
+const Admin = dynamic(() => import('@/app/Components/Admin/Admin'), {
+  ssr: false, // Optional, disables server-side rendering for this component
+  loading: () => <p className='flex items-center justify-center text-lg font-medium h-[100vh]'>Loading...</p>, // Optional loading indicator
+});
 
-// const Admin = dynamic(() => import('@/app/Components/Admin/Admin'), {
-//   ssr: false, // Optional, disables server-side rendering for this component
-//   loading: () => <p className='flex items-center justify-center text-lg font-medium h-[100vh]'>Loading...</p>, // Optional loading indicator
-// });
-
-// export default Admin;
-
-export default function AdminPage ()  {
-
-  return <Admin />
-
-}
+export default Admin;
