fa43bc37ddab63d9a4b8d99c44a00a6c8fc9d620
Vickrant <vickrantselvendran@gmail.com>
2025-05-08 10:10:38 +0530

    fix err, this sucks

--- src/app/utils/supabase.js
+++ src/app/utils/supabase.js
@@ -3,9 +3,9 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 // Use createClientComponentClient for client-side components
 export const supabase = createClientComponentClient()
 
-// import { cookies } from 'next/headers'
-// import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
-// export const createSupabaseServerClient = () => {
-//   return createServerComponentClient({ cookies })
-// }
+export const createSupabaseServerClient = () => {
+  return createServerComponentClient({ cookies })
+}
